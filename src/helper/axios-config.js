de7f2ec8c593160a8a9508599eb6565919157b04
Yeff27 <yeffer.c27@gmail.com>
2024-10-13 11:53:51 -0500

    Se cambio

--- src/helper/axios-config.js
+++ src/helper/axios-config.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    //baseURL: 'http://localhost:4000/'
-    baseURL: 'https://peliculas-n97w.onrender.com/' 
+    baseURL: 'http://localhost:4000/',
+    baseURL: 'https://peliculas-1-o25j.onrender.com' 
 });
 
 export {
